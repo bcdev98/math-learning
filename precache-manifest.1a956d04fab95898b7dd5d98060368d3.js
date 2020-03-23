caef31316f374914ca12965c0e22573eb7fc15b2
Brett Cui <bcui@bc.home>
2020-03-22 22:55:29 -0400

    Updates

---
+++ precache-manifest.1a956d04fab95898b7dd5d98060368d3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8491b809e93fa69e48a2ddce43d28ffb",
+    "url": "/math-learning/index.html"
+  },
+  {
+    "revision": "7f8758931e3cf5b9e7d8",
+    "url": "/math-learning/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "a368b242d92a88e3ec4e",
+    "url": "/math-learning/static/js/2.c47069dc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/math-learning/static/js/2.c47069dc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7f8758931e3cf5b9e7d8",
+    "url": "/math-learning/static/js/main.17d40b7b.chunk.js"
+  },
+  {
+    "revision": "782de68b06e83febc1df",
+    "url": "/math-learning/static/js/runtime-main.07d20653.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/math-learning/static/media/logo.5d5d9eef.svg"
+  }
+]);
